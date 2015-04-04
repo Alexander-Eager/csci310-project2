19548f172502a854bfe6e04f834e169cacf4985d
Ryan Zhou <ryanzhou7@gmail.com>
2015-04-04 11:31:57 -0700

    Adding test cases

---
+++ test/googleScholarSearchTest.php
@@ -0,0 +1,20 @@
+<?php
+
+// This function retrieves $numberOfResults articles that match $searchTerms,
+//	either in the document title or in the author
+// $searchTerms = title or author to search for
+// $numberOfResults = number of articles to return
+//						if less than this are returned, it should be because
+//						there weren't enough results
+// returns an array of the metadata for each article, in the form of a string
+//	we may later change this to be some kind of article objects, as required for the table.
+//	for now, the feature we are implementing is just retrieving relevant documents.
+class articles extends PHPUnit_Framework_TestCase
+{
+	public function testRetrieveArticles() {
+		$test = new 		
+	}
+
+	$this->assertEquals(  );
+}
+?>
