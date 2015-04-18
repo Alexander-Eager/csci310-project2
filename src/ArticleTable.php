fe7ee353be9f920de47d16f89d49f569be784934
Kevin Kim <kevinsk@usc.edu>
2015-04-18 16:37:09 -0700

    added article table test

--- src/ArticleTable.php
+++ src/ArticleTable.php
@@ -1,6 +1,23 @@
 <?php
 
+// A table which contains the Articles that contain $word
+
+	class ArticleTable {
+
+		public function generateTable(){
+
+		}
+
+		public function __construct(){
+
+			// POSTed data will be here to construct the ArticleTable
+		}
+
+	}
 
 
 
 ?>
+
+
+
---
+++ test/ArticleTableTest.php
@@ -0,0 +1,7 @@
+<?php
+
+
+// given an article, do i make the correct row?
+// now that i have a list of articles, do i have the correct table?
+
+?>
