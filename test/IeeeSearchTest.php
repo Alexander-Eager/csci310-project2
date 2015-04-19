457fd8d2ab5aaad1aa413f134e72647aca06251f
Haohan Tang <haohanta@usc.edu>
2015-04-18 18:04:21 -0700

    Ieeesearch test

---
+++ test/IeeeSearchTest.php
@@ -0,0 +1,75 @@
+<?php
+include('../src/IeeeSearch.php');
+
+class ArticleTest extends PHPUnit_Framework_TestCase {
+	// test the retrieve article function
+	// test title retrieve
+	public function testRetrieveArticle1() {
+		$doc = array("title" => "sample title");
+		//$a = new Article($title, $author, $date, $excerpt, $conference);
+		$b = IeeeSearch::retrieveArticle($doc);
+		$this->assertEquals("sample title", $b->getTitle());
+	}
+
+	// test author retrieve for single author
+	public function testRetrieveArticle2() {
+		$doc = array("authors" => "halfond");
+		//$a = new Article($title, $author, $date, $excerpt, $conference);
+		$b = IeeeSearch::retrieveArticle($doc);
+		$this->assertEquals("halfond", $b->getAuthors()));
+	}
+
+	// test author retrieve for multiple authors
+	public function testRetrieveArticle3() {
+		$doc = array("authors" => "halfond; sonal");
+		//$a = new Article($title, $author, $date, $excerpt, $conference);
+		$b = IeeeSearch::retrieveArticle($doc);
+		$c = array("halfond", "sonal");
+		$this->assertEquals("2", count(array_intersect($c, $b->getAuthors())));
+	}
+
+	// test date retrieve
+	public function testRetrieveArticle4() {
+		$doc = array("publishDate" => "2012-1-1");
+		//$a = new Article($title, $author, $date, $excerpt, $conference);
+		$b = IeeeSearch::retrieveArticle($doc);
+		$this->assertEquals("2012-1-1", $b->getPublishDate());
+	}
+
+	// test excerpt retrieve
+	public function testRetrieveArticle5() {
+		$doc = array("excerpt" => "halfond is great");
+		//$a = new Article($title, $author, $date, $excerpt, $conference);
+		$b = IeeeSearch::retrieveArticle($doc);
+		$this->assertEquals("halfond is great", $b->getExcerpt());
+	}
+
+	// test conference retrieve
+	public function testRetrieveArticle6() {
+		$doc = array("conference" => "halfond is great");
+		//$a = new Article($title, $author, $date, $excerpt, $conference);
+		$b = IeeeSearch::retrieveArticle($doc);
+		$this->assertEquals("halfond is great", $b->getConference());
+	}
+
+	// test search function
+	// test if the number of search result is correct
+	public function testSearch1() {
+		$a = IeeeSearch::search("halfond", "10");
+		$b = count($a);
+		$this->assertEquals(10, $b);
+	}
+
+	// test if the keywork is in title or author names
+	public function testSearch2() {
+		$a = IeeeSearch::search("halfond", "10");
+		$b = "true";
+		for($i = 0; $i < 10; $i ++){
+			if ( strpos($a[$i]->getTitle(),'halfond') == false && !in_array("halfond", $a[$i]->getAuthors() )
+				$b = "false";
+		}
+		$this->assertEquals("true", $b);
+	}
+
+}
+?>
