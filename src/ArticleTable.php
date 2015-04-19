6fa6809503b615a12d68703aa488d4bec3e3811e
Kevin Kim <kevinsk@usc.edu>
2015-04-18 20:45:38 -0700

    working table

--- src/ArticleTable.php
+++ src/ArticleTable.php
@@ -7,12 +7,17 @@
 		public static function generateTable($word) {
 
 			// API call to find articles with $word
-			// for(each article after the api call)
+
+			$output = "";
+
+			// for(each article after the api call) {
 		
-				$output = "<tr>" . "<td> article name </td>"
+				$output = $output . "<tr>" . "<td> article name </td>"
 				 					. "<td> author name </td>" 
 									. "</tr>"
 
+
+			// }					
 
 			return $output;
 
