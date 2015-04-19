47c2e94c8fc7e7514914c1cf1e2f5c49af31a325
Alexander-Eager <aeager@usc.edu>
2015-04-18 21:53:59 -0700

    Moved IEEE search functionality into master, and updated the Article class to reflect these changes. Also, included the test file (which we did write before hand on Eric Tang's laptop, but he has experienced issues uploading to GitHub.

--- src/Article.php
+++ src/Article.php
@@ -1,23 +1,51 @@
 <?php
 	class Article {
+		private $title;
+		private $authors;
+		private $publishDate;
+		private $abstract;
+		private $pubTitle;
+		private $articleNumber;
 		//returns a string of the title
 		//return null if the string is empty
-		public function getTitle();
+		public function getTitle(){
+			return $this->title;
+		}
 		//returns an array of author names as strings
-		public function getAuthors();
+		public function getAuthors(){
+			return $this->authors;
+		}
 		//returns a string of publish date
 		//should return "unknown" if the date is empty
 		//return null if no date
-		public function getPublishDate();
+		public function getPublishYear(){
+			return $this->publishDate;
+		}
 		//returns a string of excerpt
 		//return null if no excerpt
-		public function getExcerpt();
+		public function getAbstract(){
+			return $this->abstract;
+		}
 		//return a string of conference
 		//return "Unspecified" if no-conference
 		//return null if no conference
-		public function getConference();
+		public function getPubTitle(){
+			return $this->pubTitle();
+		}
+		//
+		public function getArticleNumber(){
+			return $this-?articleNumber();
+		}
 		//constructor
-		public function __construct($articleData);
+		//authors is an array
+		public function __construct($title, $authors, $publishYear, $abstract, $pubTitle, $articleNumber){
+			$this->title = $title;
+			$this->authors = $authors;
+			$this->publishYear = $publishYear;
+			$this->abstract = $abstract;
+			$this->pubTitle = $pubTitle;
+			$this->articleNumber = $articleNumber;
+		}
 
 	}
 
