c438aa0bb427a0dad71c9b9dea34a622b6dd926e
Alexander-Eager <aeager@usc.edu>
2015-04-18 21:37:40 -0700

    Finsihed code to query search

--- src/IeeeSearch.php
+++ src/IeeeSearch.php
@@ -8,7 +8,19 @@ class IeeeSearch {
 	// Parses a particular <document> tag and returns the resulting Article object
 	// returns an Article object
 	public static function retrieveArticle($documentTag) {
-		// TODO
+		// all of these are directly obtainable 
+		$title = $documentTag["title"];
+		$publicationTitle = $documentTag["pubtitle"];
+		$publicationNumber = $documentTag["punumber"];
+		$publicationYear = intval($documentTag["py"]);
+		$abstract = $documentTag["abstract"];
+		$articleNumber = $documentTag["arnumber"];
+		// getting an array of authors is a littler harder; they are separated by "; "
+		$authors = explode("; ", $documentTag["authors"]);
+
+		// now just pass all of these parameters into the constructor for a new Article
+		return new Article($title, $authors, $publishYear, $abstract, $publicationTitle, $publicationNumber,
+			$articleNumber);
 	}
 
 	// Executes a search query and returns a list of the Articles for that query
@@ -24,55 +36,72 @@ class IeeeSearch {
 
 		// convert those queries to an XML DOM tree, and then those articles to JSON, and then to arrays.
 		// the basis of this code comes from http://stackoverflow.com/questions/8830599/php-convert-xml-to-json
-		$xmlAuthor = simplexml_load_string($xmlAuthorText, null, LIBXML_NOCDATA);
-		$authorArray = array();
-		foreach ($xmlAuthor->children() as $document) {
-			// ensure that we are dealing with a "document" tag
-			if ($document->getName() !== "document") {
-				continue;
-			}
-			// convert $document to an associative array and add it to the list
-			$json = json_encode($document);
-			echo $json;
-			$article = json_decode($json, true);
-			$authorArray[] = $article;
-			var_dump($article);
-		}
+		$authorArray = retrieveListOfArticlesFromXML($xmlAuthorText);
+		$titleArray = retrieveListOfArticlesFromXML($xmlTitleText);
 
-		$xmlTitle = simplexml_load_string($xmlTitleText);
-		$titleArray = array();
-		foreach ($xmlTitle->children() as $document) {
-			// ensure that we are dealing with a "document" tag
-			if ($document->getName() !== "document") {
-				continue;
-			}
-			// convert $document to an associative array and add it to the list
-			$json = json_encode($document);
-			$article = json_decode($json, true);
-			$titleArray[] = $article;
-			var_dump($article);
-		}
-
-		var_dump($authorArray);
-		var_dump($titleArray);
-
-/*
 		// now we have $authorArray = list of articles where the author matches $searchTerms,
 		//         and $titleArray  = list of articles where the title  matches $searchTerms
 		// so combine these two lists into the articles we want to use, ignoring duplicates
 		$i = 0;
 		$numArticlesTaken = 0;
-		$authorLen = count($authorArray["root"] - 2);
+		$authorLen = count($authorArray);
 		$titleLen  = count($titleArray);
 		$ans = array();
 		while ($numArticlesTaken < $numResults) {
 			// look at adding the next article in $authorArray
 			if ($i < $authorLen) {
-				$article = IeeeSearch::retrieveArticle($authorArray[$i]);
-
+				// if it is not already in our list, add it
+				$articleToConsider = $authorArray[$i]->getArticleNumber();
+				if (!arrayHasArticle($ans, $articleToConsider)) {
+					$ans[] = $articleToConsider;
+					$numArticlesTaken ++;
+				}
+			}
+			// look at adding the next article in $titleArray
+			if ($i < $titleLen && $numArticlesTaken < $numResults) {
+				// if it is not already in our list, add it
+				$articleToConsider = $titleArray[$i]->getArticleNumber();
+				if (!arrayHasArticle($ans, $articleToConsider)) {
+					$ans[] = $articleToConsider;
+					$numArticlesTaken ++;
+				}
+			}
+			// in exceptional cases, this may happen because the search terms
+			// do not return enough results
+			if ($i >= $authorLen && $i >= $titleLen) {
+				break;
 			}
 		}
-*/
+
+		// return the answer
+		return $ans;
+	}
+
+	// function that converts a search query result into a list of articles
+	private static function retrieveListOfArticlesFromXML($xmlText) {
+		$xmlTree = simplexml_load_string($xmlAuthorText, null, LIBXML_NOCDATA);
+		$ans = array();
+		foreach ($xmlTree->children() as $document) {
+			// ensure that we are dealing with a "document" tag
+			if ($document->getName() !== "document") {
+				continue;
+			}
+			// convert $document to an associative array and add it to the list
+			$json = json_encode($document);
+			$article = json_decode($json, true);
+			$ans[] = retrieveArticle($article);
+		}
+	}
+
+	// function that determines if the given array contains the given article
+	// makes use of the article number
+	private static function arrayHasArticle($array, $article) {
+		foreach ($array as $otherArticle) {
+			if ($article->getArticleNumber() === $otherArticle->getArticleNumber()) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
 
